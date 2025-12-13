e9f385f6f2ffcaa7d48b0399e606e20029c470c1
Patricia Manzanas Tomas <pamanzan@student.42barcelona.com>
2025-12-13 20:30:55 +0100

    completed LocationConfig Class implementation

--- includes/config/LocationConfig.hpp
+++ includes/config/LocationConfig.hpp
@@ -27,6 +27,32 @@ public:
     ~LocationConfig();
 
     LocationConfig &operator=(const LocationConfig &other);
+
+    const std::string &getRoot() const;
+    const std::vector<std::string> &getIndex() const;
+    const std::vector<std::string> &getMethods() const;
+    const std::vector<std::string> &getCgiPaths() const;
+    const std::vector<std::string> &getCgiExts() const;
+    const std::map<int, std::string> &getErrorPage() const;
+    int getReturnCode() const;
+    const std::string &getReturnUrl() const;
+    size_t getBodySize() const;
+    const std::string &getPattern() const;
+    const std::string &getUploadPath() const;
+    bool getAutoindex() const;
+
+    void setRoot(const std::string &root);
+    void setIndex(const std::vector<std::string> &index);
+    void setMethods(const std::vector<std::string> &methods);
+    void setCgiPaths(const std::vector<std::string> &cgiPaths);
+    void setCgiExts(const std::vector<std::string> &cgiExts);
+    void setErrorPage(const std::map<int, std::string> &errorPage);
+    void setReturnCode(int returnCode);
+    void setReturnUrl(const std::string &returnUrl);
+    void setBodySize(size_t bodySize);
+    void setPattern(const std::string &pattern);
+    void setUploadPath(const std::string &uploadPath);
+    void setAutoindex(bool autoindex);
 };
 
 #endif
--- src/config/LocationConfig.cpp
+++ src/config/LocationConfig.cpp
@@ -36,3 +36,123 @@ LocationConfig &LocationConfig::operator=(const LocationConfig &other)
 LocationConfig::~LocationConfig()
 {
 }
+
+const std::string &LocationConfig::getRoot() const
+{
+    return _root;
+}
+
+const std::vector<std::string> &LocationConfig::getIndex() const
+{
+    return _index;
+}
+
+const std::vector<std::string> &LocationConfig::getMethods() const
+{
+    return _methods;
+}
+
+const std::vector<std::string> &LocationConfig::getCgiPaths() const
+{
+    return _cgiPaths;
+}
+
+const std::vector<std::string> &LocationConfig::getCgiExts() const
+{
+    return _cgiExts;
+}
+
+const std::map<int, std::string> &LocationConfig::getErrorPage() const
+{
+    return _errorPage;
+}
+
+int LocationConfig::getReturnCode() const
+{
+    return _returnCode;
+}
+
+const std::string &LocationConfig::getReturnUrl() const
+{
+    return _returnUrl;
+}
+
+size_t LocationConfig::getBodySize() const
+{
+    return _bodySize;
+}
+
+const std::string &LocationConfig::getPattern() const
+{
+    return _pattern;
+}
+
+const std::string &LocationConfig::getUploadPath() const
+{
+    return _uploadPath;
+}
+
+bool LocationConfig::getAutoindex() const
+{
+    return _autoindex;
+}
+
+void LocationConfig::setRoot(const std::string &root)
+{
+    _root = root;
+}
+
+void LocationConfig::setIndex(const std::vector<std::string> &index)
+{
+    _index = index;
+}
+
+void LocationConfig::setMethods(const std::vector<std::string> &methods)
+{
+    _methods = methods;
+}
+
+void LocationConfig::setCgiPaths(const std::vector<std::string> &cgiPaths)
+{
+    _cgiPaths = cgiPaths;
+}
+
+void LocationConfig::setCgiExts(const std::vector<std::string> &cgiExts)
+{
+    _cgiExts = cgiExts;
+}
+
+void LocationConfig::setErrorPage(const std::map<int, std::string> &errorPage)
+{
+    _errorPage = errorPage;
+}
+
+void LocationConfig::setReturnCode(int returnCode)
+{
+    _returnCode = returnCode;
+}
+
+void LocationConfig::setReturnUrl(const std::string &returnUrl)
+{
+    _returnUrl = returnUrl;
+}
+
+void LocationConfig::setBodySize(size_t bodySize)
+{
+    _bodySize = bodySize;
+}
+
+void LocationConfig::setPattern(const std::string &pattern)
+{
+    _pattern = pattern;
+}
+
+void LocationConfig::setUploadPath(const std::string &uploadPath)
+{
+    _uploadPath = uploadPath;
+}
+
+void LocationConfig::setAutoindex(bool autoindex)
+{
+    _autoindex = autoindex;
+}
