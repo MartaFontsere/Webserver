dd1d6178e7027cd1d2ae58680dc942b61c641edd
Patricia Manzanas Tomas <pamanzan@student.42barcelona.com>
2025-12-14 16:04:49 +0100

    Add command-line argument support for config file && main ready to merge

--- main.cpp
+++ main.cpp
@@ -1,64 +1,36 @@
 #include "Server.hpp"
-#include <iostream>
-#include <fstream>
-#include <string>
 #include "../includes/config_parser/parser/UtilsConfigParser.hpp"
 #include "../includes/config/ConfigBuilder.hpp"
-int main()
-{
-    //  Server server("8080");
-    // TODO; CONTROLAR ACCESO DE CONFIG CON Y SIN ARGUMENTO
-    // Configuration file path (relative to executable location)
-    const std::string configPath = "src/test.conf";
 
+int main(int argc, char **argv)
+{
+    std::string configPath;
+    if (argc == 1)
+        configPath = "test.conf";
+    else
+        configPath = argv[1];
+
+    Server server("8080");
     try
     {
         BlockParser root = parseAndValidateConfig(configPath);
         ConfigBuilder builder;
         std::vector<ServerConfig> servers = builder.buildFromBlockParser(root);
 
-        // Verificación básica
-        std::cout << "\n=== CONFIG LOADED SUCCESSFULLY ===" << std::endl;
-        std::cout << "Total servers: " << servers.size() << std::endl;
+        std::cout << "✅ Configuration loaded: " << servers.size() << " server(s)" << std::endl;
 
-        // Verificar que hay al menos 1 server
-        if (servers.size() > 0)
+        if (!server.init())
         {
-            std::cout << "\n--- Server 0 Details ---" << std::endl;
-            std::cout << "Port: " << servers[0].getListen() << std::endl;
-            std::cout << "Host: " << servers[0].getHost() << std::endl;
-            std::cout << "Root: " << servers[0].getRoot() << std::endl;
-            std::cout << "Max Body Size: " << servers[0].getClientMaxBodySize() << " bytes" << std::endl;
-            std::cout << "Locations: " << servers[0].getLocations().size() << std::endl;
-
-            // Mostrar server_names si hay
-            std::vector<std::string> names = servers[0].getServerNames();
-            if (names.size() > 0)
-            {
-                std::cout << "Server names: ";
-                for (size_t i = 0; i < names.size(); ++i)
-                {
-                    std::cout << names[i];
-                    if (i < names.size() - 1)
-                        std::cout << ", ";
-                }
-                std::cout << std::endl;
-            }
+            return 1;
         }
-        std::cout << "===================================\n"
-                  << std::endl;
+
+        server.run();
     }
     catch (std::exception &e)
     {
         std::cerr << "❌ Config error: " << e.what() << std::endl;
         return 1;
     }
-    // if (!server.init())
-    // {
-    //     return 1;
-    // }
-
-    // server.run();
 
     return 0;
 }
